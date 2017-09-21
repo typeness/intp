79ad507f6359b24f28761430e56c346013a38326
jerylee <jerylee1997@gmail.com>
2017-09-22 00:27:41 +0200

    Fix #12: Zaktualizować gramatykę języka w formacie EBNF. Refaktoryzacja parsera.

--- src/main/scala/io/github/typeness/intp/Parser.scala
+++ src/main/scala/io/github/typeness/intp/Parser.scala
@@ -18,9 +18,9 @@ class Parser(text: String) {
   }
 
   /*
-  program: (statement)*
+  program = {statement} ;
    */
-  def program(): Program = {
+  private def program(): Program = {
     val statemets: ListBuffer[AST] = ListBuffer.empty
     while (currentToken.tokenType != EOF && currentToken.tokenType != R_CURLY_BRACKET) {
       statemets.append(statement())
@@ -29,12 +29,12 @@ class Parser(text: String) {
   }
 
   /*
-  statement: disjunction
-             | if_statement
+  statement =   if_statement
              | while_statement
              | return_statement
+             | disjunction ;
    */
-  def statement(): AST = {
+  private def statement(): AST = {
     currentToken.tokenType match {
       case IF => ifStatement()
       case WHILE => whileStatement()
@@ -44,67 +44,57 @@ class Parser(text: String) {
   }
 
   /*
-  return_statement = RETURN disjunction
+ if_statement = IF L_ROUND_BRACKET disjunction R_ROUND_BRACKET L_CURLY_BRACKET program
+      R_CURLY_BRACKET [ELSE (L_CURLY_BRACKET program R_CURLY_BRACKET) | if_statement] ;
+  */
+  private def ifStatement(): AST = {
+    eat(IF)
+    eat(L_ROUND_BRACKET)
+    val condition = disjunction()
+    eat(R_ROUND_BRACKET)
+    eat(L_CURLY_BRACKET)
+    val ifBlock = program()
+    eat(R_CURLY_BRACKET)
+    val elseBlock = currentToken.tokenType match {
+      case ELSE =>
+        eat(ELSE)
+        eat(L_CURLY_BRACKET)
+        val elseBlock = program()
+        eat(R_CURLY_BRACKET)
+        Some(elseBlock)
+      case _ => None
+    }
+    IfAST(condition, ifBlock, elseBlock)
+  }
+
+  /*
+ while_statement = WHILE L_ROUND_BRACKET disjunction R_ROUND_BRACKET L_CURLY_BRACKET program
+      R_CURLY_BRACKET ;
+  */
+  private def whileStatement(): AST = {
+    eat(WHILE)
+    eat(L_ROUND_BRACKET)
+    val condition = disjunction()
+    eat(R_ROUND_BRACKET)
+    eat(L_CURLY_BRACKET)
+    val whileBlock = program()
+    eat(R_CURLY_BRACKET)
+    WhileAST(condition, whileBlock)
+  }
+
+  /*
+  return_statement = RETURN disjunction ;
    */
-  def returnStatement(): AST = {
+  private def returnStatement(): AST = {
     eat(RETURN)
     ReturnAST(result = disjunction())
   }
 
   /*
-  expression: term
+  disjunction = conjunction ASSIGN disjunction
+              | conjunction OR conjunction ;
    */
-  def expression(): AST = {
-    var result = term()
-    while (currentToken.tokenType == PLUS || currentToken.tokenType == MINUS) {
-      val op = currentToken
-      eat(op.tokenType)
-      result = BinOp(left = result, op = op, right = term())
-    }
-    result
-  }
-
-  /*
-  term: factor
-      | factor MULTIPLICATION factor
-      | factor DIV factor
-      | factor OR factor
-      | factor AND factor
-   */
-  def term(): AST = {
-    var result = factor()
-    while (currentToken.tokenType == MULTIPLICATION
-      || currentToken.tokenType == DIV || currentToken.tokenType == MODULO) {
-      val op = currentToken
-      eat(op.tokenType)
-      result = BinOp(left = result, op = op, right = factor())
-    }
-    result
-  }
-
-  def boolean(): AST = {
-    var result = expression()
-    while (currentToken.tokenType == GREATER || currentToken.tokenType == LESS
-      || currentToken.tokenType == GREATER_OR_EQUALS || currentToken.tokenType == LESS_OR_EQUALS
-      || currentToken.tokenType == EQUALS || currentToken.tokenType == NOT_EQUALS) {
-      val op = currentToken
-      eat(op.tokenType)
-      result = BinOp(left = result, op = op, right = expression())
-    }
-    result
-  }
-
-  def conjunction(): AST = {
-    var result = boolean()
-    while (currentToken.tokenType == AND) {
-      val op = currentToken
-      eat(op.tokenType)
-      result = BinOp(left = result, op = op, right = boolean())
-    }
-    result
-  }
-
-  def disjunction(): AST = {
+  private def disjunction(): AST = {
     var result = conjunction()
     // assignment
     if (currentToken.tokenType == ASSIGN) {
@@ -114,7 +104,6 @@ class Parser(text: String) {
           ArrayAssignAST(name, index, disjunction())
         case VarAST(name) => AssignAST(name, disjunction())
         case _ => throw new ParserError(result.token)
-
       }
     } else {
       while (currentToken.tokenType == OR) {
@@ -127,20 +116,134 @@ class Parser(text: String) {
   }
 
   /*
-  array_indexing: L_SQUARE_BRACKET expression R_SQUARE_BRACKET
+  conjunction = boolean
+              | boolean AND boolean ;
    */
-  def arrayIndexing(): AST = {
-    eat(L_SQUARE_BRACKET)
-    val index = expression()
-    eat(R_SQUARE_BRACKET)
-    index
+  private def conjunction(): AST = {
+    var result = boolean()
+    while (currentToken.tokenType == AND) {
+      val op = currentToken
+      eat(op.tokenType)
+      result = BinOp(left = result, op = op, right = boolean())
+    }
+    result
   }
 
   /*
-  function_definition:
-    FUNC formal_parameters_list L_CURLY_BRACKET program R_CURLY_BRACKET
+  boolean = expression
+          | expression GREATER expression
+          | expression LESS expression
+          | expression GREATER_OR_EQUALS expression
+          | expression LESS_OR_EQUALS expression
+          | expression EQUALS expression
+          | expression NOT_EQUALS expression ;
    */
-  def functionDefinition(): AST = {
+  private def boolean(): AST = {
+    var result = expression()
+    while (currentToken.tokenType == GREATER || currentToken.tokenType == LESS
+      || currentToken.tokenType == GREATER_OR_EQUALS || currentToken.tokenType == LESS_OR_EQUALS
+      || currentToken.tokenType == EQUALS || currentToken.tokenType == NOT_EQUALS) {
+      val op = currentToken
+      eat(op.tokenType)
+      result = BinOp(left = result, op = op, right = expression())
+    }
+    result
+  }
+
+  /*
+  expression = term
+            | term PLUS term
+            | term MINUS term ;
+   */
+  private def expression(): AST = {
+    var result = term()
+    while (currentToken.tokenType == PLUS || currentToken.tokenType == MINUS) {
+      val op = currentToken
+      eat(op.tokenType)
+      result = BinOp(left = result, op = op, right = term())
+    }
+    result
+  }
+
+  /*
+  term = factor
+      | factor MULTIPLICATION factor
+      | factor DIV factor
+      | factor MODULO factor ;
+   */
+  private def term(): AST = {
+    var result = factor()
+    while (currentToken.tokenType == MULTIPLICATION
+      || currentToken.tokenType == DIV || currentToken.tokenType == MODULO) {
+      val op = currentToken
+      eat(op.tokenType)
+      result = BinOp(left = result, op = op, right = factor())
+    }
+    result
+  }
+
+  /*
+  factor = variable
+        | function_literal
+        | unary_operator factor
+        | number_literal
+        | boolean_literal
+        | L_ROUND_BRACKET disjunction R_ROUND_BRACKET
+        | array_literal
+        | char_literal
+        | string_literal ;
+   */
+  private def factor(): AST = {
+    val token = currentToken
+    currentToken.tokenType match {
+      case ID => variable()
+      case FUNC => functionLiteral()
+      case PLUS | MINUS | NOT => unaryOperator()
+      case INTEGER_CONST | REAL_CONST => numberLiteral()
+      case TRUE | FALSE => booleanLiteral()
+      case L_ROUND_BRACKET =>
+        eat(L_ROUND_BRACKET)
+        val result = disjunction()
+        eat(R_ROUND_BRACKET)
+        result
+      case L_SQUARE_BRACKET => arrayLiteral()
+      case APOSTROPHE => characterLiteral()
+      case QUOTATION => stringLiteral()
+      case _ => throw ParserError(token)
+    }
+  }
+
+  /*
+  variable = ID (actual_parameters_list)*
+          | ID (array_indexing)*
+          | ID ;
+  */
+  private def variable(): AST = {
+    val name = currentToken.value
+    eat(ID)
+    currentToken.tokenType match {
+      case L_ROUND_BRACKET =>
+        var functionCall = FunctionCall(VarAST(IdToken(name)), actualParametersList())
+        while (currentToken.tokenType == L_ROUND_BRACKET) {
+          functionCall = FunctionCall(source = functionCall, actualParametersList())
+        }
+        functionCall
+      //        FunctionCall(source = VarAST(IdToken(name)), actualParametersList())
+      case L_SQUARE_BRACKET =>
+        var arrayAccess = ArrayAccess(VarAST(IdToken(name)), arrayIndexing())
+        while (currentToken.tokenType == L_SQUARE_BRACKET) {
+          arrayAccess = ArrayAccess(source = arrayAccess, arrayIndexing())
+        }
+        arrayAccess
+      case _ => VarAST(name = IdToken(name))
+    }
+  }
+
+  /*
+  function_literal =
+    FUNC formal_parameters_list L_CURLY_BRACKET program R_CURLY_BRACKET ;
+   */
+  private def functionLiteral(): AST = {
     eat(FUNC)
     val parameters = formalParametersList()
     eat(L_CURLY_BRACKET)
@@ -150,10 +253,140 @@ class Parser(text: String) {
   }
 
   /*
-  formal_parameters_list: L_ROUND_BRACKET ID (COMMA ID)* R_ROUND_BRACKET
-                        | L_ROUND_BRACKET R_ROUND_BRACKET
+ unary_operator = PLUS factor
+               | MINUS factor
+               | NOT factor ;
+  */
+  private def unaryOperator(): AST = {
+    currentToken.tokenType match {
+      case PLUS | MINUS | NOT =>
+        val token = currentToken
+        eat(currentToken.tokenType)
+        UnaryOp(op = token, expr = factor())
+      case _ => throw new ParserError(currentToken)
+    }
+  }
+
+  /*
+  number_literal = INTEGER_CONST | REAL_CONST ;
    */
-  def formalParametersList(): List[IdToken] = {
+  private def numberLiteral(): AST = {
+    val token = currentToken
+    currentToken.tokenType match {
+      case INTEGER_CONST =>
+        eat(INTEGER_CONST)
+        Number(IntegerConstToken(token.value.toInt))
+      case REAL_CONST =>
+        eat(REAL_CONST)
+        Number(RealConstToken(token.value.toDouble))
+      case _ => throw new ParserError(currentToken)
+    }
+  }
+
+  /*
+  boolean_literal = TRUE | FALSE ;
+   */
+  private def booleanLiteral(): AST = {
+    currentToken.tokenType match {
+      case TRUE =>
+        eat(TRUE)
+        BooleanLiteral(TrueToken)
+      case FALSE =>
+        eat(FALSE)
+        BooleanLiteral(FalseToken)
+      case _ => throw new ParserError(currentToken)
+    }
+  }
+
+  /*
+  def array_literal = L_SQUARE_BRACKET disjunction (COMMA disjunction)* R_SQUARE_BRACKET
+                   | L_SQUARE_BRACKET R_SQUARE_BRACKET ;
+   */
+  private def arrayLiteral(): AST = {
+    eat(L_SQUARE_BRACKET)
+    currentToken.tokenType match {
+      case R_SQUARE_BRACKET =>
+        eat(R_SQUARE_BRACKET)
+        ArrayLiteral(List.empty)
+      case _ =>
+        val result: ListBuffer[AST] = ListBuffer(disjunction())
+        while (currentToken.tokenType == COMMA) {
+          eat(COMMA)
+          result.append(disjunction())
+        }
+        eat(R_SQUARE_BRACKET)
+        ArrayLiteral(result.toList)
+    }
+  }
+
+  /*
+  character_literal = APOSTROPHE CHAR APOSTROPHE ;
+   */
+
+  private def characterLiteral(): AST = {
+    eat(APOSTROPHE)
+    currentToken match {
+      case ch: CharToken =>
+        val result = CharLiteral(ch)
+        eat(CHARACTER)
+        eat(APOSTROPHE)
+        result
+      case _ => throw new ParserError(currentToken)
+    }
+  }
+
+  /*
+  string_literal = QUOTATION STRING QUOTATION ;
+   */
+
+  private def stringLiteral(): AST = {
+    eat(QUOTATION)
+    currentToken match {
+      case str: StringToken =>
+        val result = ArrayLiteral(stringToListOfChars(str))
+        eat(STRING)
+        eat(QUOTATION)
+        result
+      case _ => throw new ParserError(currentToken)
+    }
+  }
+
+  /*
+  actual_parameters_list = L_ROUND_BRACKET disjunction (COMMA disjunction)*
+                  | L_ROUND_BRACKET R_ROUND_BRACKET ;
+   */
+  private def actualParametersList(): List[AST] = {
+    eat(L_ROUND_BRACKET)
+    currentToken.tokenType match {
+      case R_ROUND_BRACKET =>
+        eat(R_ROUND_BRACKET)
+        List.empty
+      case _ =>
+        val result: ListBuffer[AST] = ListBuffer(disjunction())
+        while (currentToken.tokenType == COMMA) {
+          eat(COMMA)
+          result.append(disjunction())
+        }
+        eat(R_ROUND_BRACKET)
+        result.toList
+    }
+  }
+
+  /*
+  array_indexing = L_SQUARE_BRACKET expression R_SQUARE_BRACKET ;
+   */
+  private def arrayIndexing(): AST = {
+    eat(L_SQUARE_BRACKET)
+    val index = expression()
+    eat(R_SQUARE_BRACKET)
+    index
+  }
+
+  /*
+  formal_parameters_list = L_ROUND_BRACKET ID (COMMA ID)* R_ROUND_BRACKET
+                        | L_ROUND_BRACKET R_ROUND_BRACKET ;
+   */
+  private def formalParametersList(): List[IdToken] = {
     eat(L_ROUND_BRACKET)
     currentToken match {
       case token: IdToken =>
@@ -176,208 +409,8 @@ class Parser(text: String) {
     }
   }
 
-  /*
-  if_statement = IF L_ROUND_BRACKET disjunction R_ROUND_BRACKET L_CURLY_BRACKET program
-       R_CURLY_BRACKET [ELSE L_CURLY_BRACKET program R_CURLY_BRACKET]
-   */
-  def ifStatement(): AST = {
-    eat(IF)
-    eat(L_ROUND_BRACKET)
-    val condition = disjunction()
-    eat(R_ROUND_BRACKET)
-    eat(L_CURLY_BRACKET)
-    val ifBlock = program()
-    eat(R_CURLY_BRACKET)
-    val elseBlock = currentToken.tokenType match {
-      case ELSE =>
-        eat(ELSE)
-        eat(L_CURLY_BRACKET)
-        val elseBlock = program()
-        eat(R_CURLY_BRACKET)
-        Some(elseBlock)
-      case _ => None
-    }
-    IfAST(condition, ifBlock, elseBlock)
-  }
-
-  /*
- while_statement = WHILE L_ROUND_BRACKET disjunction R_ROUND_BRACKET L_CURLY_BRACKET program
-      R_CURLY_BRACKET
-  */
-  def whileStatement(): AST = {
-    eat(WHILE)
-    eat(L_ROUND_BRACKET)
-    val condition = disjunction()
-    eat(R_ROUND_BRACKET)
-    eat(L_CURLY_BRACKET)
-    val whileBlock = program()
-    eat(R_CURLY_BRACKET)
-    WhileAST(condition, whileBlock)
-  }
-
-  /*
-  factor: variable
-        | unary_operator factor
-        | literal
-        | L_ROUND_BRACKET expression R_ROUND_BRACKET
-   */
-
-  def factor(): AST = {
-    val token = currentToken
-    currentToken.tokenType match {
-      case ID => variable()
-      case FUNC => functionDefinition()
-      case PLUS | MINUS | NOT =>
-        eat(currentToken.tokenType)
-        UnaryOp(op = token, expr = factor())
-      case INTEGER_CONST =>
-        eat(INTEGER_CONST)
-        Number(IntegerConstToken(token.value.toInt))
-      case REAL_CONST =>
-        eat(REAL_CONST)
-        Number(RealConstToken(token.value.toDouble))
-      case TRUE =>
-        eat(TRUE)
-        BooleanLiteral(TrueToken)
-      case FALSE =>
-        eat(FALSE)
-        BooleanLiteral(FalseToken)
-      case L_ROUND_BRACKET =>
-        eat(L_ROUND_BRACKET)
-        val result = disjunction()
-        eat(R_ROUND_BRACKET)
-        result
-      case L_SQUARE_BRACKET =>
-        arrrayLiteral()
-      case APOSTROPHE => characterLiteral()
-      case QUOTATION => stringLiteral()
-      case _ => throw ParserError(token)
-    }
-  }
-
   private def stringToListOfChars(str: StringToken): List[AST] =
     str.value.map(c => CharLiteral(CharToken(c))).toList
-
-  /*
-  string_literal: QUOTATION CHAR QUOTATION
-   */
-
-  def stringLiteral(): AST = {
-    eat(QUOTATION)
-    currentToken match {
-      case str: StringToken =>
-        val result = ArrayLiteral(stringToListOfChars(str))
-        eat(STRING)
-        eat(QUOTATION)
-        result
-      case _ => throw new ParserError(currentToken)
-    }
-  }
-
-  /*
-  character_literal: APOSTROPHE CHAR APOSTROPHE
-   */
-
-  def characterLiteral(): AST = {
-    eat(APOSTROPHE)
-    currentToken match {
-      case ch: CharToken =>
-        val result = CharLiteral(ch)
-        eat(CHARACTER)
-        eat(APOSTROPHE)
-        result
-      case _ => throw new ParserError(currentToken)
-    }
-  }
-
-  /*
-  actual_parameters_list: L_ROUND_BRACKET disjunction (COMMA disjunction)*
-                  | L_ROUND_BRACKET R_ROUND_BRACKET
-   */
-  def actualParametersList(): List[AST] = {
-    eat(L_ROUND_BRACKET)
-    currentToken.tokenType match {
-      case R_ROUND_BRACKET =>
-        eat(R_ROUND_BRACKET)
-        List.empty
-      case _ =>
-        val result: ListBuffer[AST] = ListBuffer(disjunction())
-        while (currentToken.tokenType == COMMA) {
-          eat(COMMA)
-          result.append(disjunction())
-        }
-        eat(R_ROUND_BRACKET)
-        result.toList
-    }
-  }
-
-  /*
-  def array_literal: L_SQUARE_BRACKET disjunction (COMMA disjunction)* R_SQUARE_BRACKET
-                   | L_SQUARE_BRACKET R_SQUARE_BRACKET
-   */
-  def arrrayLiteral(): AST = {
-    eat(L_SQUARE_BRACKET)
-    currentToken.tokenType match {
-      case R_SQUARE_BRACKET =>
-        eat(R_SQUARE_BRACKET)
-        ArrayLiteral(List.empty)
-      case _ =>
-        val result: ListBuffer[AST] = ListBuffer(disjunction())
-        while (currentToken.tokenType == COMMA) {
-          eat(COMMA)
-          result.append(disjunction())
-        }
-        eat(R_SQUARE_BRACKET)
-        ArrayLiteral(result.toList)
-    }
-  }
-
-  /*
-  variable: ID
-          | ID (actual_parameters_list)*
-          | ID (array_indexing)*
-   */
-  private def variable(): AST = {
-    val name = currentToken.value
-    eat(ID)
-    currentToken.tokenType match {
-      case L_ROUND_BRACKET =>
-        var functionCall = FunctionCall(VarAST(IdToken(name)), actualParametersList())
-        while (currentToken.tokenType == L_ROUND_BRACKET) {
-          functionCall = FunctionCall(source = functionCall, actualParametersList())
-        }
-        functionCall
-//        FunctionCall(source = VarAST(IdToken(name)), actualParametersList())
-      case L_SQUARE_BRACKET =>
-        var arrayAccess = ArrayAccess(VarAST(IdToken(name)), arrayIndexing())
-        while (currentToken.tokenType == L_SQUARE_BRACKET) {
-          arrayAccess = ArrayAccess(source = arrayAccess, arrayIndexing())
-        }
-        arrayAccess
-      case _ => VarAST(name = IdToken(name))
-    }
-  }
-
-  /*
-  literal: number_literal
-        | array_literal
-        | boolean_literal
-        | character_literal
-        | string_literal
-   */
-  /*
-  unary_operator: PLUS
-                | MINUS
-                | NOT
-   */
-  def unaryOperator(): AST = {
-    currentToken.tokenType match {
-      case PLUS | MINUS | NOT =>
-        eat(currentToken.tokenType)
-        UnaryOp(op = currentToken, expr = factor())
-      case _ => throw new ParserError(currentToken)
-    }
-  }
 
   private def eat(tokenType: TokenType): Unit = {
     if (currentToken.tokenType == tokenType) {
