72272c0a528b8f15feb7f6fce3f7632f4fa08ef1
jerylee <jerylee1997@gmail.com>
2017-11-05 00:36:21 +0100

    WIP #4: Funkcje wbudowane readInt/readDouble/readLine.

--- src/main/scala/io/github/typeness/intp/BuiltinFunctions.scala
+++ src/main/scala/io/github/typeness/intp/BuiltinFunctions.scala
@@ -7,7 +7,7 @@ object BuiltinFunctions {
                    position: Position) =>
                     arg match {
                       case seq: Seq[_] => println("[" + seq.mkString(", ") + "]")
-//                      case seq: Seq[Char] => println(seq)
+                      //                      case seq: Seq[Char] => println(seq)
                       case x => println(x)
                     }),
     "size" -> ((arg: Any, compilationUnit: CompilationUnit, position: Position) => {
@@ -28,6 +28,49 @@ object BuiltinFunctions {
         case seq: Seq[_] => "[" + seq.mkString(", ") + "]"
         case value       => value.toString
       }
-    })
+    }),
+    "readInt" -> ((arg: Any,
+                   compilationUnit: CompilationUnit,
+                   position: Position) =>
+                    ZeroArgumentFunction(
+                      arg,
+                      compilationUnit,
+                      position,
+                      "readInt",
+                      () => scala.io.StdIn.readInt()
+                    )),
+    "readDouble" -> ((arg: Any,
+                      compilationUnit: CompilationUnit,
+                      position: Position) =>
+                       ZeroArgumentFunction(
+                         arg,
+                         compilationUnit,
+                         position,
+                         "readDouble",
+                         () => scala.io.StdIn.readDouble()
+                       )),
+    "readLine" -> ((arg: Any,
+                    compilationUnit: CompilationUnit,
+                    position: Position) =>
+                     ZeroArgumentFunction(
+                       arg,
+                       compilationUnit,
+                       position,
+                       "readLine",
+                       () => scala.io.StdIn.readLine()
+                     ))
   )
+
+  private object ZeroArgumentFunction {
+    def apply(arg: Any,
+              compilationUnit: CompilationUnit,
+              position: Position,
+              name: String,
+              fn: () => Any): Any = arg match {
+      case seq: Seq[_] if seq.isEmpty => fn()
+      case s: Seq[_]                  => throw WrongFunctionCall(name, s.size, 0, compilationUnit, position)
+      case _                          => throw WrongFunctionCall(name, 1, 0, compilationUnit, position)
+    }
+  }
+
 }
